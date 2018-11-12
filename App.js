92ebbd689f8a507181df27d1417a87145f230628
Sean O'Connor <sean@michaels-imac.home>
2018-11-12 22:11:21 +0000

    Header component added

--- App.js
+++ App.js
@@ -7,36 +7,15 @@
  */
 
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { } from 'react-native';
+import Header from './src/components/header';
 
 
 export default class App extends Component {
-  render() {
+
+render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>Press Cmd+R to reload + Cmd+D or shake for dev menu</Text>
-      </View>
+      <Header />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
