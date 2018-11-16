f0dc6f39ba50dbc2b09c717b1c5ad345d0699fe4
Sean O'Connor <sean@michaels-imac.home>
2018-11-16 23:33:28 +0000

    Button added

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
 
@@ -34,6 +35,9 @@ const AlbumDetail = ({ album }) => {
           style={imageStyle}
           source={{ uri: image }}
         />
+      </CardSection>
+      <CardSection>
+        <Button />
       </CardSection>
     </Card>
   );
